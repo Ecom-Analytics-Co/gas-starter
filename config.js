993fd5f2eb6e77b641c192e24455e173fddffa44
ecomanalytics <74625979+ecomanalytics@users.noreply.github.com>
2021-03-02 20:13:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-// this file is used for file IDs, API keys, etc.
-const API_KEY = "abc123";
+// this file is used for YOUR file IDs, API keys, etc.
+// 1) REMEMBER TO git update-index --assume-unchanged config.js after cloning the repo
+// 2) REMEMBER TO git update-index --assume-unchanged .clasp.json after cloning the repo
 
 // DEV script project ID: ???
+const API_KEY = "abc123";
 
 // PROD script project ID: ???
