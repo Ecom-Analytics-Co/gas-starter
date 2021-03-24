d438ec507e9d1e1f82375e86eea125c0604bdea1
Haidar Ali <haidarzsh@gmail.com>
2021-03-25 01:06:28 +0500

    Update to prettify cfg

--- config.js
+++
@@ -1,8 +0,0 @@
-// this file is used for YOUR file IDs, API keys, etc.
-// 1) REMEMBER TO git update-index --assume-unchanged config.js after cloning the repo
-// 2) REMEMBER TO git update-index --assume-unchanged .clasp.json after cloning the repo
-
-// DEV script project ID: ???
-const API_KEY = "abc123";
-
-// PROD script project ID: ???
